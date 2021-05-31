fd806ef2e1b83445c213a309ca069ea651adcfd7
DurgaPrasadAG <68811536+DurgaPrasadAG@users.noreply.github.com>
2021-05-31 16:46:18 +0530

    Implemented Straight forward algorithm to find max and min items.

--- src/StraightMaxMin.java
+++ src/StraightMaxMin.java
@@ -1,3 +1,37 @@
-public class StraightMaxMin {
-    
+class StraightMaxMin {
+    int max;
+    int min;
+
+    /**
+     * Implementation of Straight Forward algorithm
+     * @param num - Array of numbers
+     */
+    public void straightMaxMin(int[] num) {
+        int max;
+        int min;
+        max = min = num[0];
+
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] > max) {
+                max = num[i];
+            }
+            if (num[i] < min){
+                min = num[i];
+            }
+        }
+
+        maxMin(max, min);
+    }
+
+    void maxMin(int max, int min) {
+        System.out.println("Max element : " + max);
+        System.out.println("Min element : " + min);
+    }
+
+    public static void main(String[] args) {
+        int[] num = {22, 13, -5, -8, 15, 60, 17, 31, 47};
+        
+        var findMaxAndMin = new StraightMaxMin();
+        findMaxAndMin.straightMaxMin(num);
+    }
 }
